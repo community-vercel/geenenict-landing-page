aa84b75f5e3abe7c3884ae36188dd87a1fcbf001
community-vercel <community-vercel>
2025-03-03 08:49:18 -0800

    aa

--- app/page.js
+++ app/page.js
@@ -80,6 +80,7 @@ export default function Home() {
       </p>
     </div>
 
+
     <div className="bg-gray-700 bg-opacity-50 p-6 rounded-lg shadow-md border-l-4 border-orange-500">
       <h3 className="text-2xl font-semibold text-white mb-3">Key Expertise</h3>
       <ul className="text-gray-300 space-y-2">
