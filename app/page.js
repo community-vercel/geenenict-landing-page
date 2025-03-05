d0a7e995913ed7802670e4c6462f0d55745a3d51
community-vercel <community-vercel>
2025-03-04 17:51:40 -0800

    aaa

--- app/page.js
+++ app/page.js
@@ -28,6 +28,7 @@ async function fetchData(endpoint) {
       cache: 'no-store',
     });
 
+    
     if (!response.ok) {
       throw new Error(`Failed to fetch data from ${endpoint}: ${response.statusText}`);
     }
