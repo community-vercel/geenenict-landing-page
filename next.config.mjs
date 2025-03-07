8afa930387755145bd8953cb50b768fcce8bcdbe
community-vercel <community-vercel>
2025-03-07 13:40:49 -0800

    hjj

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
+import TerserPlugin from 'terser-webpack-plugin';
+
 const nextConfig = {
+    webpack(config, { isServer }) {
+        if (!isServer) {
+          config.optimization.minimizer.push(
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true, // Optional: Remove console logs
+                },
+              },
+            })
+          );
+        }
+        return config;
+      },
+
     images:{
 domains:['geenenict-server.vercel.app',
-    'geenenict-server.vercel.apphttps'
+    'geenenict-server.vercel.app '
 ]
     }
 
